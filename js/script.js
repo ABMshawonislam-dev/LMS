ee4f92a9a4e1ba45ef9e791158f74691d219ce01
ll-alamin-ll <ll.alamin.ll@gmail.com>
2020-08-22 15:01:45 +0600

    done

---
+++ js/script.js
@@ -0,0 +1,43 @@
+(function(){
+    $('.counter').counterUp({
+        delay: 10,
+        time: 1000
+    });
+
+    $('.feedback').slick({
+        dots: true,
+        infinite: false,
+        arrows:false,
+        speed: 300,
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        responsive: [
+          {
+            breakpoint: 992,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 1,
+              infinite: true,
+              dots: true
+            }
+          },
+          {
+            breakpoint: 768,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 1
+            }
+          },
+          {
+            breakpoint: 576,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+          // You can unslick at a given breakpoint now by adding:
+          // settings: "unslick"
+          // instead of a settings object
+        ]
+      });
+})()
